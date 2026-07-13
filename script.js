ed269fd45b1e6de3633968e2d6144f7033cf5c52
utkx4sh-spec <utkx4sh@gmail.com>
2026-07-13 22:52:15 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+entries.forEach(entry => {
+if(entry.isIntersecting){
+entry.target.style.opacity = "1";
+entry.target.style.transform = "translateY(0)";
+}
+});
+});
+
+cards.forEach(card => {
+card.style.opacity = "0";
+card.style.transform = "translateY(40px)";
+card.style.transition = "0.6s ease";
+observer.observe(card);
+});
+
+const navLinks = document.querySelectorAll(".navbar a");
+
+navLinks.forEach(link => {
+link.addEventListener("click", () => {
+document.querySelector(".navbar").classList.remove("active");
+});
+});
+
+console.log("Website Loaded Successfully");
+
+});
